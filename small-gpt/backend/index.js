3904db83a7fb9efdca77b4da0cc6860a3de4fc7a
Gowtha-dev <56531397+Gowtha-dev@users.noreply.github.com>
2023-11-30 03:27:48 +0530

    init commit

---
+++ small-gpt/backend/index.js
@@ -0,0 +1,39 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  apiKey: "",  //replace key here
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a EbereGPT. You can help with graphic design tasks",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+//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
